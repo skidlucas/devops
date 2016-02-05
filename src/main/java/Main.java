376293b1f05b543caeaa298aa510b044088c97c5
Lucas MARTINEZ <martinez.lucas@etu.unice.fr>
2016-02-05 08:49:41 +0100

    juste pour avoir les dossiers

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Lucas MARTINEZ
+ * @version 05/02/16
+ */
+public class Main {
+    void main(){
+        System.out.println("coucou");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,6 @@
+/**
+ * @author Lucas MARTINEZ
+ * @version 05/02/16
+ */
+public class MainTest {
+}
