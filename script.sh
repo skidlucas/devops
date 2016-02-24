1f6b44e88dc759149ef6bb71fab535e5fc72beab
Lucas MARTINEZ <martinez.lucas@etu.unice.fr>
2016-02-24 15:37:46 +0100

    script ajoute le processeur dynamiquement

--- script.sh
+++ script.sh
@@ -15,16 +15,26 @@ if [ $# -eq 1 ]
 		echo -e "Vous avez indiqué que le path de votre projet est le suivant : $1\n"
 		echo "Nettoyage préalable..." #faire les vérifs nécessaires
 		rm -rf ./transformation-code/*
-		mkdir ./transformation-code/src/
+		mkdir -p ./transformation-code/src/
+		#cp pomTransfoDefault.xml ./transformation-code/pom.xml #TODO: garder le pom de transfo-code par défaut, puis ajouter les dépendances du source à importer
 		echo -e "OK\n"
 
 		echo "Création des liens symboliques..." #faire les vérifs nécessaires
 		ln -sv $1/src/* ./transformation-code/src/
-		ln -sv $1/pom.xml ./transformation-code/pom.xml
+		ln -sv $1/pom.xml ./transformation-code/pom.xml #TODO suite: au lieu de lier le pom directement comme ici
 		echo -e "OK\n"
 
+		#On ajoute le processeur (un seul pour l'instant, déjà connu) dans le pom.xml
+		PROC="OperatorToMinusProcessor"
+		sed -i "s/\(<processor>\).*\(<\/processor>\)/\1$PROC\2/" ./transformation-code/pom.xml
+
+		#On lance mvn clean test qui va se servir de spoon pour muter le code source fourni en paramètre du script
 		mvn clean test
+
+		#On exécute un script python pour lire le rapport XML des tests et le transformer en HTML
 		./python/readXML.py
+
+		#On affiche ce rapport avec le browser par défaut
 		xdg-open ./python/rapMutant.html &
 	else
 		echo "ERREUR: trop d'argument, veuillez indiquer uniquement le chemin absolu de votre code à muter en paramètre."
